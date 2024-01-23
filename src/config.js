f8ef3f7aadacfd7ecfce174e6d2746271cd42011
R3GT-hub <saransh2002sharma@gmail.com>
2024-01-23 21:44:07 +0530

    Login/Signup

---
+++ src/config.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const connect = mongoose.connect("mongodb://localhost:27017/Togethr");
+
+connect.then(() => {
+    console.log("Database connected successfully");
+})
+.catch(() => {
+    console.log("Error in connecting to the database");
+});
+
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {  // Added missing colon here
+        type: String,
+        required: true
+    }
+});
+
+const collection =new mongoose.model("Togethr",LoginSchema,"Togethr");
+module.exports=collection;
